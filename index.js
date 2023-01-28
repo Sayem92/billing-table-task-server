926ab38227993fc92e01eac44620560f1996bb25
Sayem92 <sayemmiha123@gmail.com>
2023-01-29 02:42:35 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware--
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        
+
+
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+
+run().catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+    res.send('Online billing Page server is running.............');
+});
+
+app.listen(port, () => {
+    console.log('Online billing Page server running on:', port);
+})
