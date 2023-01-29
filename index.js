b1cf5d7f376667c9d12365d81cce2e938a59292c
Sayem92 <sayemmiha123@gmail.com>
2023-01-29 23:52:35 +0600

    get single bill for update

--- index.js
+++ index.js
@@ -67,7 +67,13 @@ async function run() {
             res.send(result);
         });
 
-
+        //get billing list ---------
+        app.get('/update-billing/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await billingListCollection.findOne(query);
+            res.send(result);
+        });
 
 
 
